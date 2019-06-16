4419b4780c1d5f18b07306d5eb34e8d01b54034c
Edvaldo Mantovanelli <ecmantovanelli@gmail.com>
2019-06-16 08:39:01 -0300

    resizing images

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
+const app = express();
 
 mongoose.connect('mongodb+srv://edvaldo:edvaldo@cluster0-ynd4p.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true
 });
 
-const app = express();
-
+app.use('/file', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 app.use(require('./routes'));
 
 app.listen('3333');
