7b317fcffff59618ae163ee0a847e9a7ccdc1afd
Edvaldo Mantovanelli <ecmantovanelli@gmail.com>
2019-06-16 11:00:50 -0300

    Criando o header e estruturando as rotas do cabecalho

---
+++ frontend/src/pages/Feed.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import './Feed.css';
+
+class Feed extends Component {
+    render(){
+        return (
+            <section id="post-list">
+
+            </section>
+        )
+    }
+}
+
+export default Feed;
