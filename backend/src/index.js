b91706f9dfd1ec51037b9e0c7f02d48b65e5a837
Edvaldo Mantovanelli <ecmantovanelli@gmail.com>
2019-06-16 08:54:20 -0300

    adicionando socket

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,33 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
 
 const app = express();
 
-mongoose.connect('mongodb+srv://edvaldo:edvaldo@cluster0-ynd4p.mongodb.net/test?retryWrites=true&w=majority', {
+const server = require("http").Server(app);
+
+const io = require("socket.io")(server);
+
+mongoose.connect(
+  "mongodb+srv://edvaldo:edvaldo@cluster0-ynd4p.mongodb.net/test?retryWrites=true&w=majority",
+  {
     useNewUrlParser: true
+  }
+);
+
+app.use((req, res, next) => {
+    req.io = io;
+
+    next();
 });
 
-app.use('/file', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
-app.use(require('./routes'));
+app.use(cors());
 
-app.listen('3333');
+app.use(
+  "/file",
+  express.static(path.resolve(__dirname, "..", "uploads", "resized"))
+);
+app.use(require("./routes"));
+
+server.listen("3333");
